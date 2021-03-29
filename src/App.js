39c1de5e12edd6fd6dd110e06f9ba33907541695
NorouziM <mnorouzidev@gmail.com>
2021-03-30 01:37:14 +0430

    change broswer router to hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 
 import "./App.css";
